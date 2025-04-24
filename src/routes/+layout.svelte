be8c8401b3907c3b55db4aab23c37952f6d64b13
J. Kim <kjfootman@daum.net>
2025-04-24 15:34:40 +0900

    2025-04-24

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script>
+    let { data, children } = $props();
+</script>
+
+<svelte:head>
+    <title>Sloth</title>
+</svelte:head>
+
+<header>
+    <h1>this is a header</h1>
+</header>
+
+<main>
+    <nav></nav>
+    {@render children()}
+</main>
+
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+    }
+
+    header {
+        width: 100%;
+    }
+
+    nav {
+        width: 100%;
+        background-color: aqua;
+    }
+
+    main {
+        width: 100%;
+        height: 500px;
+        background-color: aliceblue;
+        /* box-sizing: border-box; */
+
+        display: grid;
+        grid-template-columns: 400px calc(100% - 400px);
+    }
+</style>
