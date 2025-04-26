5f05532a6d20cd580f1dc4c8d67050d8d4f10b53
kjm <kjfootman@daum.net>
2025-04-26 23:57:25 +0900

    fs module replaced by read of /server

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -3,12 +3,14 @@ export const config = {
     runtime: 'nodejs22.x'
 };
 
-import fs from 'fs';
+// import fs from 'fs';
+import { read } from '$app/server'
 import YAML from 'yaml'
 
 /** @type {import('./$types').LayoutServerLoad} */
-export function load() {
-    const file = fs.readFileSync('static/menu.yaml', 'utf8');
+export async function load() {
+    // const file = fs.readFileSync('static/menu.yaml', 'utf8');
+    const file = await read('static/menu.yaml').text();
 
     return {
         menu: YAML.parse(file)
