2e589cc04c5a1979a51ba9810e8aa2b918501f28
kjm <kjfootman@daum.net>
2025-05-01 00:22:14 +0900

    Prismjs 적용

---
+++ src/routes/numerics/bisection/+page.server.js
@@ -0,0 +1,17 @@
+import { error } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+    const url = "https://raw.githubusercontent.com/kjfootman/jm_math/main/src/error.rs";
+    
+
+    const res = await fetch(url);
+
+    if (!res.ok) error(404, {message: 'Not found'});
+
+    const text = await res.text();
+
+    return {
+        code: text
+    }
+}
--- src/routes/numerics/bisection/+page.svelte
+++ src/routes/numerics/bisection/+page.svelte
@@ -1,1 +1,25 @@
+<script>
+    import Prism from "@magidoc/plugin-svelte-prismjs";
+    import 'prismjs/components/prism-rust';
+    import 'prismjs/themes/prism-tomorrow.css';
+
+    let { data } = $props();
+</script>
+
 <h1>Bisection Method</h1>
+
+<div style="width: 80%;">
+    <Prism 
+        language={'rust'} 
+        source={data.code}
+        showCopyButton={true}
+        showLineNumbers={true}
+    />
+</div>
+
+<!-- <code>{data.code}</code> -->
+ <!-- {#await data.code}
+    <p>waitting</p>
+ {:then code} 
+    <code class="language-rust">let x = 3;</code>
+ {/await} -->
