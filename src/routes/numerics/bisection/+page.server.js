c58165e39eac97c6989350b169d121097b9efe02
JoengMin <kjfootman@gmail.com>
2025-05-08 10:45:43 +0900

    test to read rust code

--- src/routes/numerics/bisection/+page.server.js
+++ src/routes/numerics/bisection/+page.server.js
@@ -1,11 +1,12 @@
 import { error } from '@sveltejs/kit';
-// import { read } from '$app/server';
 import some_file from '$lib/somefile.txt';
+import { read } from '$app/server';
+// import rust from '$lib/rust_codes/src/bisection.rs';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch }) {
-	const url = 'https://raw.githubusercontent.com/kjfootman/blog/main/rust_codes/src/bisection.rs';
-	const res = await fetch(url);
+	const code_url = 'https://raw.githubusercontent.com/kjfootman/blog/main/rust_codes/src/bisection.rs';
+	const res = await fetch(code_url);
 
 	if (!res.ok) error(404, { message: 'Not found' });
 
@@ -18,13 +19,20 @@ export async function load({ fetch }) {
 	// const item = await test2.text()
 	// console.log(item);
 
-	const test2 = await fetch('/rust_codes/src/bisection.rs');
-	const item = await test2.text()
-	console.log(item);
+	// const test2 = await fetch(rust);
+	// const item = await test2.text()
+	// console.log(item);
+
+	// let tmp = await fetch('/src/lib/rust_codes/src/bisection.rs');
+	// let test = await tmp.text();
+	// console.log(test);
+
+	const test = await read('/src/lib/rust_codes/src/bisection.rs').text();
+	console.log(test);
 
 	return {
-		code: item,
+		code: text,
 		// html: html
-		text: item
+		text: test
 	};
 }
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps: {
-		exclude: [
-			'D:/workspace/svelte/blog/node_modules/.vite/deps/chunk-6FCZEMHD.js?v=3f9fbe49',
-			'D:/workspace/svelte/blog/node_modules/.vite/deps/chunk-KTP3TDPN.js?v=84e1820c'
-		]
-	}
+	// optimizeDeps: {
+	// 	exclude: [
+	// 		'D:/workspace/svelte/blog/node_modules/.vite/deps/chunk-6FCZEMHD.js?v=3f9fbe49',
+	// 		'D:/workspace/svelte/blog/node_modules/.vite/deps/chunk-KTP3TDPN.js?v=84e1820c'
+	// 	]
+	// }
 });
